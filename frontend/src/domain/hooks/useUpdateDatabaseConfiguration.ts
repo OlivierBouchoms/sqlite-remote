19e3bb979e258f90b7f794f3ca76cb50962c88ba
Olivier Bouchoms <olivier_2000@live.nl>
2026-01-03 16:12:10 +0100

    style: run prettier

--- frontend/src/domain/hooks/useUpdateDatabaseConfiguration.ts
+++ frontend/src/domain/hooks/useUpdateDatabaseConfiguration.ts
@@ -25,7 +25,7 @@ export const useUpdateDatabaseConfiguration = () => {
                 const next = old.slice();
 
                 next[index] = newConfig;
-                
+
                 return next;
             });
         },
