a890e0a2beda4c1d61379e2906539c89eda3df82
Olivier Bouchoms <olivier_2000@live.nl>
2026-01-03 15:15:39 +0100

    feat: execute selected command text if text is selected

--- frontend/src/components/queryConsole/index.tsx
+++ frontend/src/components/queryConsole/index.tsx
@@ -1,8 +1,8 @@
 import { useTranslation } from 'react-i18next';
 import { Toolbar, ToolbarItemProps } from '../toolbar';
-import { useMemo, useState } from 'react';
+import { useCallback, useMemo, useRef, useState } from 'react';
 import { FaCopy, FaPlay } from 'react-icons/fa6';
-import CodeMirror from '@uiw/react-codemirror';
+import CodeMirror, { ReactCodeMirrorRef } from '@uiw/react-codemirror';
 import styles from './index.module.css';
 import { sql, SQLite } from '@codemirror/lang-sql';
 import { useServerServicePostApiServerQuery } from '../../generated/api/queries';
@@ -17,26 +17,44 @@ type Props = {
 export const QueryConsole = ({ open }: Props) => {
     const [content, setContent] = useState<string>('');
 
+    const codeMirrorElement = useRef<ReactCodeMirrorRef | null>();
+
     const { data: postQueryData, mutateAsync: postQuery, isPending: isPostingQuery, error: postQueryError } = useServerServicePostApiServerQuery();
 
     const { selectedConfig } = useDatabaseConfiguration();
 
     const { t } = useTranslation(undefined, { keyPrefix: 'components.queryConsole' });
 
+    const getCommandTextToExecute = useCallback(() => {
+        const editorView = codeMirrorElement.current?.view;
+
+        if (editorView) {
+            const selection = editorView.state.selection.main;
+            if (selection.from !== selection.to) {
+                return editorView.state.doc.sliceString(selection.from, selection.to);
+            }
+        }
+
+        return content;
+    }, [content]);
+
+    const onExecute = useCallback(async () => {
+        if (!selectedConfig) return;
+
+        const commandText = getCommandTextToExecute().trim();
+
+        if (commandText.length === 0) {
+            return;
+        }
+        await postQuery({ requestBody: { dbPath: selectedConfig.dbPath, sshHost: selectedConfig.ssh.hostName, commandText: commandText } });
+    }, [getCommandTextToExecute, postQuery, selectedConfig]);
+
     const toolbarItems = useMemo((): ToolbarItemProps[] => {
         return [
             {
                 icon: <FaPlay />,
                 label: t('toolbar.execute'),
-                onClick: async () => {
-                    if (!selectedConfig) return;
-
-                    const query = content.trim();
-                    if (query.length === 0) {
-                        return;
-                    }
-                    await postQuery({ requestBody: { dbPath: selectedConfig.dbPath, sshHost: selectedConfig.ssh.hostName, commandText: query } });
-                },
+                onClick: onExecute,
                 disabled: !content.trim().length || isPostingQuery,
             },
             {
@@ -54,6 +72,7 @@ export const QueryConsole = ({ open }: Props) => {
             <Toolbar items={toolbarItems} />
             <div className={styles.editorWrapper}>
                 <CodeMirror
+                    ref={(e) => (codeMirrorElement.current = e)}
                     className={styles.editor}
                     height='100%'
                     lang='sql'
